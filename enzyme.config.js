d1d4d41a3012d728fb5df54b9fdc144bf514a2ea
Collin Newman <collin.newman37@gmail.com>
2021-04-01 13:27:02 -0400

    Fixed enzmye setup

--- enzyme.config.js
+++ enzyme.config.js
@@ -1,4 +1,4 @@
-const Enzyme = require('Enzyme');
+const { configure } = require('enzyme');
 const Adapter = require('@wojtekmaj/enzyme-adapter-react-17');
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
