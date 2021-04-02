daade49a73f2cd462f947691375f55f6b31678e2
Collin Newman <collin.newman37@gmail.com>
2021-04-02 10:13:42 -0400

    Added a catch block incase of an error when connecting to mongodb

--- database/index.ts
+++ database/index.ts
@@ -3,7 +3,10 @@ require('dotenv').config();
 
 const uri = process.env.MONGO || 'mongodb://localhost/mvp';
 
-const connection = mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+const connection = mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+  .catch(err => {
+    console.log('Error on initial mongoose connection', err);
+  });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
