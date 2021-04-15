a1fdb2e482c6bf20d0dd249330b7c5d826b25755
Collin Newman <collin.newman37@gmail.com>
2021-04-14 22:55:25 -0400

    Fixed webpack env variable error

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,13 @@ const dotenv = require('dotenv');
 const webpack = require('webpack');
 
 const env = dotenv.config().parsed;
+console.log(env);
 const envKeys = Object.keys(env).reduce((prev, next) => {
     prev[`process.env.${next}`] = JSON.stringify(env[next]);
     return prev;
 }, {});
+
+console.log(envKeys);
 
 module.exports = {
   mode: "development",
